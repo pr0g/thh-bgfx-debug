ea5044cf72b8aea017a6c34899542a69a678ac4b
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2021-02-07 15:39:53 +0000

    start of adding wire debug cube

---
+++ include/thh-bgfx-debug/debug-cube.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "as/as-math-ops.hpp"
+#include "debug-vertex.hpp"
+
+#include <vector>
+
+namespace dbg
+{
+
+class DebugCubes
+{
+  static DebugVertex CubeVertices[];
+  static uint16_t CubeIndices[];
+
+  bgfx::VertexBufferHandle cube_vbh_;
+  bgfx::IndexBufferHandle cube_ibh_;
+  bgfx::ProgramHandle program_handle_;
+  bgfx::ViewId view_;
+
+  struct CubeInstance
+  {
+    CubeInstance() = default;
+    CubeInstance(const as::mat4& transform, const as::vec4& color)
+      : transform_(transform), color_(color)
+    {
+    }
+
+    as::mat4 transform_;
+    as::vec4 color_;
+  };
+
+  std::vector<CubeInstance> instances_;
+
+public:
+  static void init();
+
+  DebugCubes(bgfx::ViewId view, bgfx::ProgramHandle program_handle);
+  ~DebugCubes();
+
+  void reserveCubes(size_t count);
+  void addCube(const as::mat4& transform, const as::vec4& color);
+  void submit();
+};
+
+inline void DebugCubes::addCube(
+  const as::mat4& transform, const as::vec4& color)
+{
+  instances_.emplace_back(transform, color);
+}
+
+} // namespace dbg
---
+++ src/debug-cube.cpp
@@ -0,0 +1,83 @@
+#include "thh-bgfx-debug/debug-cube.hpp"
+
+namespace dbg
+{
+
+static constexpr int CubeVerticesCount = 8;
+static constexpr int CubeIndicesCount = 4; // 24
+
+DebugVertex DebugCubes::CubeVertices[CubeVerticesCount] = {
+  {-0.5f, 0.5f, 0.5f, 0xff000000},   {0.5f, 0.5f, 0.5f, 0xff000000},
+  {-0.5f, -0.5f, 0.5f, 0xff000000},  {0.5f, -0.5f, 0.5f, 0xff000000},
+  {-0.5f, 0.5f, -0.5f, 0xff000000},  {0.5f, 0.5f, -0.5f, 0xff000000},
+  {-0.5f, -0.5f, -0.5f, 0xff000000}, {0.5f, -0.5f, -0.5f, 0xff000000}
+};
+
+uint16_t DebugCubes::CubeIndices[CubeIndicesCount] = {
+  0, 1, 3, 2
+}
+
+void DebugCubes::init()
+{
+}
+
+DebugCubes::DebugCubes(
+  const bgfx::ViewId view, const bgfx::ProgramHandle program_handle)
+  : view_(view), program_handle_(program_handle)
+{
+  cube_vbh_ = bgfx::createVertexBuffer(
+    bgfx::makeRef(CubeVertices, sizeof(CubeVertices)), DebugVertex::Layout);
+  cube_ibh_ = bgfx::createIndexBuffer(
+    bgfx::makeRef(CubeIndices, sizeof(CubeIndices)));
+}
+
+DebugCubes::~DebugCubes()
+{
+  bgfx::destroy(cube_vbh_);
+  bgfx::destroy(cube_ibh_);
+}
+
+void DebugCubes::reserveCubes(const size_t count)
+{
+  instances_.reserve(count);
+}
+
+void DebugCubes::submit()
+{
+  // 80 bytes stride = 64 bytes for mat4 + 16 bytes for RGBA color.
+  const uint16_t instance_stride = 80;
+
+  if (
+    !instances_.empty()
+    && bgfx::getAvailInstanceDataBuffer(instances_.size(), instance_stride)
+         == instances_.size()) {
+    bgfx::InstanceDataBuffer idb;
+    bgfx::allocInstanceDataBuffer(&idb, instances_.size(), instance_stride);
+
+    uint8_t* data = idb.data;
+    for (const auto& cube_instance : instances_) {
+      auto* inst_transform = (float*)data;
+      std::copy(
+        as::mat_const_data(cube_instance.transform_),
+        as::mat_const_data(cube_instance.transform_) + 16, inst_transform);
+
+      auto* color = (float*)&data[64];
+      color[0] = cube_instance.color_[0];
+      color[1] = cube_instance.color_[1];
+      color[2] = cube_instance.color_[2];
+      color[3] = cube_instance.color_[3];
+
+      data += instance_stride;
+    }
+
+    bgfx::setVertexBuffer(view_, cube_vbh_);
+    bgfx::setIndexBuffer(cube_ibh_);
+
+    bgfx::setInstanceDataBuffer(&idb);
+
+    bgfx::setState(BGFX_STATE_DEFAULT | BGFX_STATE_PT_LINESTRIP);
+    bgfx::submit(view_, program_handle_);
+  }
+}
+
+} // namespace dbg
